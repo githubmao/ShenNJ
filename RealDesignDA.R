ec7ceb821ecafb85b3877dee15d65eeab8c2a3d7
githubmao <maoyanbit@foxmail.com>
2018-08-12 21:23:44 +0800

    real design DA

--- RealDesignDA.R
+++ RealDesignDA.R
@@ -16,21 +16,51 @@ setwd(dir = "E:/R/ShenNJ/Data/realDesign")  # 设置工作目录
 
 
 # drivingTraj
-df.drivingtrajR12V20VRTdown <- fread("drivingTraj_R12_V20_VRT-1.58.txt",
-                                     header = TRUE,
-                                     sep = "auto",
-                                     stringsAsFactors = FALSE,
-                                     data.table = FALSE,
-                                     skip = "Xo",
-                                     col.names = c("posX", "posY"))
+# df.drivingtrajR12V20VRTdown
+tmp.df <- read.table(file = "drivingTraj_R12_V20_VRT-1.58.txt",
+                     header = TRUE,
+                     sep = "",
+                     stringsAsFactors = FALSE,
+                     skip = 1,
+                     col.names = c("posX", "posY"))
 
-df.drivingtrajR12V20VRTup <- fread("drivingTraj_R12_V20_VRT1.58.txt",
-                                   header = TRUE,
-                                   sep = "auto",
-                                   stringsAsFactors = FALSE,
-                                   data.table = FALSE,
-                                   skip = "Xo",
-                                   col.names = c("posX", "posY"))
+kXDesignRowNo <- which(tmp.df$posX == "X_Design")
+kXTargetRowNo <- which(tmp.df$posX == "X_Target")
+
+tmp.df1 <- tmp.df[1:(kXDesignRowNo - 1),]
+tmp.df1$TrajTyp <- "DrivingTraj"
+
+tmp.df2 <- tmp.df[(kXTargetRowNo + 1):length(tmp.df$posX),]
+tmp.df2$TrajTyp <- "TargetTraj"
+
+df.drivingtrajR12V20VRTdown <- rbind(tmp.df1, tmp.df2)
+
+
+# df.drivingtrajR12V20VRTup
+tmp.df <- read.table(file = "drivingTraj_R12_V20_VRT1.58.txt",
+                     header = TRUE,
+                     sep = "",
+                     stringsAsFactors = FALSE,
+                     skip = 1,
+                     col.names = c("posX", "posY"))
+
+kXDesignRowNo <- which(tmp.df$posX == "X_Design")
+kXTargetRowNo <- which(tmp.df$posX == "X_Target")
+
+tmp.df1 <- tmp.df[1:(kXDesignRowNo - 1),]
+tmp.df1$TrajTyp <- "DrivingTraj"
+
+tmp.df2 <- tmp.df[(kXTargetRowNo + 1):length(tmp.df$posX),]
+tmp.df2$TrajTyp <- "TargetTraj"
+
+df.drivingtrajR12V20VRTup <- rbind(tmp.df1, tmp.df2)
+
+
+df.drivingtrajR12V20VRTdown$posX <- as.numeric(df.drivingtrajR12V20VRTdown$posX)
+df.drivingtrajR12V20VRTdown$posY <- as.numeric(df.drivingtrajR12V20VRTdown$posY)
+
+df.drivingtrajR12V20VRTup$posX <- as.numeric(df.drivingtrajR12V20VRTup$posX)
+df.drivingtrajR12V20VRTup$posY <- as.numeric(df.drivingtrajR12V20VRTup$posY)
 
 
 # latForCoeff
@@ -108,7 +138,33 @@ df.trackOffR12V20VRTup <- fread("trackOff_R12_V20_VRT1.58.txt",
 
 
 # 1 RAD, 12m, Down, Right----
-# 1.1 drivingTraj & longSpeed----?????????????
+# 1.1 drivingTraj & longSpeed----
+plot.drivingtrajR12V20VRTdown <- ggplot(data = df.drivingtrajR12V20VRTdown,
+                                        aes(x = posX, y = posY)) +
+  geom_path(aes(colour = factor(TrajTyp)), size = 1) +
+  scale_x_continuous(limits = c(-12, 0)) +
+  scale_y_continuous(limits = c(-12, 12)) +
+  theme(legend.position = c(0.8, 0.3),
+        legend.title = element_blank(),
+        axis.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 10))
+
+plot.drivingtrajR12V20VRTdown
+
+plot.longspeedR12V20VRTdown <- ggplot(data = df.longspeedR12V20VRTdown,
+                                      aes(x = Station, y = Speed)) +
+  geom_line(size = 1) +
+  scale_x_continuous(limits = c(0, 40), breaks = seq(0, 38, 5)) +
+  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, 5)) +
+  theme(axis.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 10))
+
+plot.longspeedR12V20VRTdown
+
+grid.arrange(plot.drivingtrajR12V20VRTdown, plot.longspeedR12V20VRTdown,
+             ncol=2, nrow=1)
+
+
 # 1.2 trackoff----
 plot.trackoffR12V20VRTdown <- ggplot(data = df.trackOffR12V20VRTdown,
                                      aes(x = Station, y = TrackOff)) +
@@ -118,6 +174,8 @@ plot.trackoffR12V20VRTdown <- ggplot(data = df.trackOffR12V20VRTdown,
         axis.title = element_text(face = "bold", size = 10))
 
 plot.trackoffR12V20VRTdown
+
+max(df.trackOffR12V20VRTdown$TrackOff)
 
 
 # 1.3 latforcoeff----
@@ -137,7 +195,33 @@ plot.latforcoeffR12V20VRTdown
 
 
 # 2 RAD, 12m, Up, Left----
-# 2.1 drivingTraj & longSpeed----?????????????
+# 2.1 drivingTraj & longSpeed----
+plot.drivingtrajR12V20VRTup <- ggplot(data = df.drivingtrajR12V20VRTup,
+                                      aes(x = posX, y = posY)) +
+  geom_path(aes(colour = factor(TrajTyp)), size = 1) +
+  scale_x_continuous(limits = c(-15, 0)) +
+  scale_y_continuous(limits = c(-15, 15)) +
+  theme(legend.position = c(0.8, 0.3),
+        legend.title = element_blank(),
+        axis.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 10))
+
+plot.drivingtrajR12V20VRTup
+
+plot.longspeedR12V20VRTup <- ggplot(data = df.longspeedR12V20VRTup,
+                                    aes(x = Station, y = Speed)) +
+  geom_line(size = 1) +
+  scale_x_continuous(limits = c(0, 40), breaks = seq(0, 38, 5)) +
+  scale_y_continuous(limits = c(0, 30), breaks = seq(0, 30, 5)) +
+  theme(axis.text = element_text(face = "bold", size = 10),
+        axis.title = element_text(face = "bold", size = 10))
+
+plot.longspeedR12V20VRTup
+
+grid.arrange(plot.drivingtrajR12V20VRTup, plot.longspeedR12V20VRTup,
+             ncol=2, nrow=1)
+
+
 # 2.2 trackoff----
 plot.trackoffR12V20VRTup <- ggplot(data = df.trackOffR12V20VRTup,
                                    aes(x = Station, y = TrackOff)) +
@@ -147,6 +231,8 @@ plot.trackoffR12V20VRTup <- ggplot(data = df.trackOffR12V20VRTup,
         axis.title = element_text(face = "bold", size = 10))
 
 plot.trackoffR12V20VRTup
+
+max(df.trackOffR12V20VRTup$TrackOff)
 
 
 # 2.3 latforcoeff----
